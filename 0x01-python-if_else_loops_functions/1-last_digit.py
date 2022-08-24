8bdd0aebda4a954b371b489f8327ba05c6e646a9
root <root@LAPTOP-F3SH5AR2.localdomain>
2022-08-23 18:08:03 -0700

    if-else

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-1000, 1000)
+last_digit = (number % 10)
+
+if last_digit is 0:
+    conditionalStr = "0"
+elif last_digit > 5:
+    conditionalStr = "greater thn 5"
+else
+    conditionalStr = "less than 6 and not 0"
+
+print(f"Last digit of {number} is {last_digit} and is {coditionalStr}")
