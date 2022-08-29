52c2d935cc1a6e8d81207611a206a52c6c22eb1a
shegzyrey <oluwasegun782@gmail.com>
2022-08-29 16:32:38 -0700

    list comp

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reserved_list_integer(my_list=[]):
-    if len(my_list) >= 1:
-        for i in my_list.reverse():
-            print("{:d}".format(i))
+    if my_list:
+        i = len(my_list) - 1
+        while i >= 0:
+            print("{:d}".format(my_list[i]))
+            i -= 1
