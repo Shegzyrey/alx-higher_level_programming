b9d45b5492b963cd6d86cb1a68250d5e344d2054
shegzyrey <oluwasegun782@gmail.com>
2022-08-31 17:37:00 -0700

    data structure update

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for k in sorte(a_dictionary.keys()):
-        print("{0}: {1}".format(k, a_dictionary[k]))
+        return("{0}: {1}".format(k, a_dictionary[k]))
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    n_dictionary = a_dictionary.copy()
-    for i in n_dictionary:
-        n_dictionary[i] = n_dictionary[i] * 2
-    return (n_dictionary)
+    return(dict(map(lambda i: (i[0], i[1] * 2), a_dictionary.items())))
