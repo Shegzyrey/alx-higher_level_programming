a65c18fa49eefd885427380d1fdb805a77af0242
“shegzyrey” <oluwasegun782@gmail.com>
2024-06-02 18:44:09 -0400

    updated insert query

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,8 @@
 -- create and insert
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
-INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
-INSERT INTO second_table (id, name, score) VALUES (2, "Bob", 14);
-INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
+INSERT INTO second_table (id, name, score)
+VALUES
+(1, "John", 10),
+(2, "Alex", 3),
+(2, "Bob", 14),
+(4, "George", 8);
