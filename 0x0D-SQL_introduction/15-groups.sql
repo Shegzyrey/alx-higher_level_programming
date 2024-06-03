9858ccad1eebf4bb5d5230f52ba4778357e8e0b9
“shegzyrey” <oluwasegun782@gmail.com>
2024-06-03 12:22:14 -0400

    count of records

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- count the frequency of a record
-SELECT score, COUNT(score) number FROM second_table GROUP BY score DESC;
+SELECT score, COUNT(score) number FROM second_table GROUP BY score ORDER BY 2 DESC;
