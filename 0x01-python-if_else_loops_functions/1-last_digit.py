bf808ead986bf86ffcc9ea1f0687487d3f09a707
“shegzyrey” <oluwasegun782@gmail.com>
2024-06-04 08:39:24 -0400

    used unary operation

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,12 +5,13 @@ l_digit = (number % 10)
 varStr1 = "and is greater than 5"
 varStr2 = "and is 0"
 varStr3 = "and is less than 6 and not 0"
+STR = "Last digit of"
 if (l_digit > 5):
-    print("Last digit of {} is {} {}".format(number, l_digit, varStr1))
+    print("{} {} is {} {}".format(STR,number, l_digit, varStr1))
 elif (l_digit == 0):
-    print("Last digit of {} is {} {}".format(number, l_digit, varStr2))
+    print("{} {} is {} {}".format(STR,number, l_digit, varStr2))
 elif (number < 0):
-    print("Last digit of {} is {} {}".format(number, (number * -1) % 10, varStr3))
+    print("{} {} is {} {}".format(STR,number, (-number % 10), varStr3))
 else:
-    print("Last digit of {} is {} {}" .format(number, l_digit, varStr3))
+    print("{} {} is {} {}" .format(STR,number, l_digit, varStr3))
 
