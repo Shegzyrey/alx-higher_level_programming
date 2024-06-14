e1e97ea5ebec6a69a0c0395adeab12329f0f2009
“shegzyrey” <oluwasegun782@gmail.com>
2024-06-14 09:42:56 -0400

    fixed index erro

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    if idx < (len(my_list) - 1) and idx > 0:
+    '''funtion to edit list in place'''
+    if idx < (len(my_list) - 1) and idx >= 0:
         my_list[idx] = element
         return (my_list)
