28483f89980db318b31231423f1e532f7cd696b0
“shegzyrey” <oluwasegun782@gmail.com>
2024-06-14 09:45:30 -0400

    fixed return statement

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     '''funtion to edit list in place'''
-    if idx < (len(my_list) - 1) and idx >= 0:
+    if idx <= (len(my_list) - 1) and idx >= 0:
         my_list[idx] = element
         return (my_list)
     return (my_list)
