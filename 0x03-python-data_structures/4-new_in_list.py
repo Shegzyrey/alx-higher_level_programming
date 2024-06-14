7751508538c0a4c413433a89435f5a81e5e67c26
“shegzyrey” <oluwasegun782@gmail.com>
2024-06-14 10:10:33 -0400

    created a new lst

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if (idx < len(my_list) - 1) and (idx > -1)
+    if idx <= (len(my_list) - 1) and idx >= 0:
         new_list = my_list[:]
         new_list[idx] = element
-    return(new_list)
+        return (new_list)
+    return (my_list)
