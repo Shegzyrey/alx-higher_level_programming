4cef84162fa2d2be8e4b048e50ca41df96bda562
“shegzyrey” <oluwasegun782@gmail.com>
2024-06-20 18:28:23 -0400

    class rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+class Rectangle {constructor (width, height){
+    if (w > 0 && Number.isInteger(w) && h > 0 && Number.isInteger(h)){
+        this.width = w;
+        this.height = h;
+        } else {
+            this.width = undefined;
+            this.height = undefined;
+        }
+    }
+    print() {
+        for (let i = 0; i < this.width; i++) {
+            let row = "";
+            for (let j = 0; j < this.height; j++) {
+                row += "x";
+            }
+            console.log(row)
+        }
+    }
+}
+
