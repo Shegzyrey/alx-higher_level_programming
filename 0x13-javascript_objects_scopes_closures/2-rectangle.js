3972f9124b0e190355efd85d5b4891291152a41a
“shegzyrey” <oluwasegun782@gmail.com>
2024-06-21 10:21:06 -0400

    semistandard rules

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-class Rectangle {constructor (w, h){
-    if (w > 0 && Number.isInteger(w) && h > 0 && Number.isInteger(h)){
-    this.width = w;
-    this.height = h;
-    } else {
-        this.width = undefined;
-        this.height = undefined;
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    }
-};
+  }
+}
 
 module.exports = Rectangle;
