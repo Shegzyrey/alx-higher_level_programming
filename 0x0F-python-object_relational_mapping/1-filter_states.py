d1a39b50e157fdb99ecd4769da527e11185f3373
shegzyrey <rey@DESKTOP-P49K6KF.localdomain>
2024-06-23 16:51:37 -0700

    added 1-filter_states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""List all states in ascending order of ID"""
+
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    if len(sys.argv) != 4:
+        res = "Usage: ./0-states.py <mysql_username>"
+        res += "<mysql_password> <db_name>"
+        print(res)
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=mysql_username,
+                         passwd=mysql_password,
+                         db=db_name)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name like 'N%' ORDER BY states.id ASC;")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
