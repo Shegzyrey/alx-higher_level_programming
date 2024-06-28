8fd84b17bc0937ac889e5916f6a5710cfd66c0a8
shegzyrey <rey@DESKTOP-P49K6KF.localdomain>
2024-06-28 05:16:46 -0700

    fixed column issues

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,17 +5,17 @@
 if __name__ == "__main__":
 
     import MySQLdb
-    import sys
+    from sys import argv
 
     if len(sys.argv) != 4:
         res = "Usage: ./0-states.py <mysql_username>"
         res += "<mysql_password> <db_name>"
         print(res)
 
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
-    state = sys.argv[4]
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    db_name = argv[3]
+    claus = argv[4]
 
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=mysql_username,
@@ -23,8 +23,8 @@ if __name__ == "__main__":
                          db=db_name)
 
     cursor = db.cursor()
-    sql_query = "SELECT * FROM states WHERE states.name = '{}'\
-            ORDER BY states.id ASC".format(state)
+    sql_query = "SELECT id,name FROM states WHERE states.name = '{}'\
+            ORDER BY states.id ASC".format(claus)
     cursor.execute(sql_query)
     rows = cursor.fetchall()
     for row in rows:
