490f86529b40607c6b41bcc6567610bc1318a0b2
shegzyrey <rey@DESKTOP-P49K6KF.localdomain>
2024-06-28 06:40:04 -0700

    safe filtering

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""List all states in ascending order of ID"""
+
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    from sys import argv
+
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    db_name = argv[3]
+    claus = argv[4]
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=mysql_username,
+                         passwd=mysql_password,
+                         db=db_name)
+
+    cursor = db.cursor()
+    sql_query = "SELECT * FROM states WHERE states.name LIKE\
+            BINARY %(name)s ORDER BY states.id ASC".format(claus)
+    cursor.execute(sql_query, {'name': claus})
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
